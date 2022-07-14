263c7edc9789b99b147a3df3a3588f5d94fb4129
Aaron Manaloto <mail@aapomm.io>
2022-07-14 15:43:55 +0800

    Redirect on CSV selection only and preserve project URL

--- app/assets/javascripts/dradis/plugins/csv/upload.js
+++ app/assets/javascripts/dradis/plugins/csv/upload.js
@@ -1,3 +1,10 @@
 window.addEventListener('job-done', function(e){
-  window.location.href = '/projects/1/csv/upload/new'
+  var uploader = document.getElementById('uploader');
+
+  if (uploader.value === 'Dradis::Plugins::CSV') {
+    var path = window.location.pathname;
+    var project_path = path.split('/').slice(0, -1).join('/');
+
+    window.location.href = project_path + '/csv/upload/new';
+  }
 });
