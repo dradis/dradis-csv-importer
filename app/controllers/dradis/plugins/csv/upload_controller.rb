023a18db789f9c3cc9018ef0de7ee613f58b8aa6
sean-yeoh <sean.yeoh@hotmail.com>
2022-07-14 20:32:35 +0800

    Importer v1

--- app/controllers/dradis/plugins/csv/upload_controller.rb
+++ app/controllers/dradis/plugins/csv/upload_controller.rb
@@ -3,6 +3,35 @@ module Dradis::Plugins::CSV
     include ProjectScoped
 
     def new
+      issue_mappings = {
+       'Vulnerability Name' => 'Title',
+       'Severity' => 'Severity',
+       'Vulnerability Category' => 'Category',
+       'CVSS Base' => 'CVSSv3.BaseScore',
+       'Vulnerability Impact' => 'Impact',
+       'Vulnerability Vendor Reference' => 'Reference',
+       'Solution Information' => 'Solution',
+      }
+
+      evidence_mappings = {
+        'OS' => 'OS',
+        'Port' => 'Port'
+      }
+
+      node_column = 'Host address'
+      csv_id_column = 'Holm Identifier'
+
+      attachment = Rails.root.join('HOLM-INFRA.csv')
+      byebug
+      MappingService.import(
+        project: current_project,
+        file: attachment,
+        issue_mappings: issue_mappings,
+        evidence_mappings: evidence_mappings,
+        csv_id_column: csv_id_column,
+        node_column: node_column
+      )
+
       job_id = params[:job_id].to_i
       filename = Resque.redis.get(job_id)
 
---
+++ app/services/dradis/plugins/csv/mapping_service.rb
@@ -0,0 +1,55 @@
+require 'csv'
+
+module Dradis::Plugins::CSV
+  class MappingService
+    class << self
+      def import(params = {})
+        @csv_id_column = params[:csv_id_column]
+        @evidence_mappings = params[:evidence_mappings]
+        @issue_mappings = params[:issue_mappings]
+        @node_column = params[:node_column]
+        @project = params[:project]
+
+        begin
+          CSV.foreach(params[:file], headers: true) do |row|
+            process_row(row)
+          end
+          logger.info{ "CSV processed." }
+        rescue ::CSV::MalformedCSVError => e
+          error = "The CSV seems to be malformed: #{e.message}."
+          logger.fatal { error }
+          content_service.create_note(text: error)
+          false
+        end
+      end
+
+      private
+
+      def content_service
+        @content_service ||= Dradis::Plugins::ContentService::Base.new(
+          project: @project,
+          plugin: Dradis::Plugins::CSV
+        )
+      end
+
+      def process_row(row)
+        data = row.to_h
+        csv_id = data[@csv_id_column]
+        node = data[@node_column]
+
+        issue_text = @issue_mappings.map do |column, field|
+          "#[#{field}]#\n#{data[column]}"
+        end.join("\n\n")
+
+        issue = content_service.create_issue(text: issue_text, id: csv_id)
+        node = content_service.create_node(label: node, type: :host)
+
+        evidence_content = @evidence_mappings.map do |column, field|
+          "#[#{field}]#\n#{data[column]}"
+        end.join("\n\n")
+
+        content_service.create_evidence(issue: issue, node: node, content: evidence_content)
+      end
+    end
+  end
+end
