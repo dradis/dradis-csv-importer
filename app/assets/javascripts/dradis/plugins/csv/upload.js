47f878d22df62882ce805cac4a46d3e907dc4e92
Aaron Manaloto <mail@aapomm.io>
2022-07-14 19:44:36 +0800

    Use Redis to pass filename data with the jobId

--- app/assets/javascripts/dradis/plugins/csv/upload.js
+++ app/assets/javascripts/dradis/plugins/csv/upload.js
@@ -1,10 +1,12 @@
 window.addEventListener('job-done', function(e){
-  var uploader = document.getElementById('uploader');
+  var jobId = e.detail.job_id,
+      uploader = document.getElementById('uploader');
 
   if (uploader.value === 'Dradis::Plugins::CSV') {
     var path = window.location.pathname;
     var project_path = path.split('/').slice(0, -1).join('/');
 
-    window.location.href = project_path + '/csv/upload/new';
+    var redirectPath  = project_path + '/csv/upload/new?job_id=' + jobId;
+    window.location.href = redirectPath;
   }
 });
--- lib/dradis/plugins/csv/importer.rb
+++ lib/dradis/plugins/csv/importer.rb
@@ -5,7 +5,20 @@ module Dradis::Plugins::CSV
     end
 
     def import(params={})
+      logger.info { "Validating uploaded file..." }
+
+      if File.extname(params[:file]).downcase != '.csv'
+        logger.info { 'Invalid file' }
+        return false
+      end
+
       logger.info { 'Parsing CSV file...' }
+
+      uid = @logger.uid
+
+      # SEE: app/controllers/dradis/plugins/csv/upload_controller.rb
+      Resque.redis.set(uid, File.basename(params[:file]))
+
       logger.info { 'Done' }
     end
   end
