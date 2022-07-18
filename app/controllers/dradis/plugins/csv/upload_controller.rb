8c0856dd89130af80422de645fb7675b71d00236
Aaron Manaloto <mail@aapomm.io>
2022-07-18 15:07:22 +0800

    Add CSV header parsing

--- app/controllers/dradis/plugins/csv/upload_controller.rb
+++ app/controllers/dradis/plugins/csv/upload_controller.rb
@@ -3,10 +3,17 @@ module Dradis::Plugins::CSV
     include ProjectScoped
 
     def new
+      parse_csv
+    end
+
+    private
+
+    def parse_csv
       job_id = params[:job_id].to_i
       filename = Resque.redis.get(job_id)
+      attachment = Attachment.find(filename, conditions: { node_id: current_project.plugin_uploads_node.id })
 
-      @attachment = Attachment.find(filename, conditions: { node_id: current_project.plugin_uploads_node.id })
+      @headers = ::CSV.open(attachment.fullpath, &:readline)
     end
   end
 end
