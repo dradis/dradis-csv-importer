eb71625dd191803b40a212e756d4092d38491ad3
sean-yeoh <sean.yeoh@hotmail.com>
2022-07-21 20:08:22 +0800

    Use strong params

--- app/controllers/dradis/plugins/csv/upload_controller.rb
+++ app/controllers/dradis/plugins/csv/upload_controller.rb
@@ -17,7 +17,7 @@ module Dradis::Plugins::CSV
 
       MappingImportJob.perform_later(
         file: @attachment.fullpath.to_s,
-        id_index: mappings_params[:identifier],
+        id_index: params[:identifier],
         mappings: mappings_params[:mappings].to_h,
         project_id: current_project.id,
         uid: params[:log_uid].to_i
@@ -44,7 +44,7 @@ module Dradis::Plugins::CSV
     end
 
     def mappings_params
-      params.permit(:identifier, mappings: [:field, :type])
+      params.require(:mappings).permit(field_attributes: [:field, :type])
     end
   end
 end
